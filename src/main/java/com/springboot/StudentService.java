45e0f3b6a095a4a26fcdad12f3abb5fd7e04fe0b
Darren Armstrong <armstrong.d1990@gmail.com>
2017-06-23 11:58:12 +0100

    Student Adding functionality
    Added functionality to Add students to the student application

--- src/main/java/com/springboot/StudentService.java
+++ src/main/java/com/springboot/StudentService.java
@@ -11,5 +11,13 @@ public class StudentService {
 	public HashMap<Long, Student> getAllStudents(){
 		return Application.hmStudent;
 	}
+	
+	@RequestMapping(value="/add", method = RequestMethod.POST)
+	public Student addStudent(@RequestParam(value="name") String name,
+			@RequestParam(value="subject", defaultValue = "unknown") String subject){
+		Student student = new Student(name, subject);
+		Application.hmStudent.put(new Long(student.getId()), student);
+		return student;
+	}
 
 }
